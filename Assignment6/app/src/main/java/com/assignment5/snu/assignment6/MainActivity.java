3c1c4644749efbb280167ae2feab7cfa9ca859f0
sandeeptharavattath <sandeeptv.ndr@gmal.com>
2017-03-05 20:26:56 +0530

    Android Assignment5

---
+++ Assignment6/app/src/main/java/com/assignment5/snu/assignment6/MainActivity.java
@@ -0,0 +1,13 @@
+package com.assignment5.snu.assignment6;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
